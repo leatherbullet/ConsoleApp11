cffd9aa4f9b06ae4da26a6919cfd67ad92d0a903
79612 <79612@DESKTOP-O1P5TC5>
2024-02-19 07:25:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int armor = 50;
+            int damagePerSecond = 5;
+            uint money = 100;
+            int heals = 100;
+            ushort stamina = 100;
+            sbyte morale = 100;
+            string appeal = "привет";
+            bool isDead = heals <= 0;
+            bool isTired = stamina <= 0;
+            bool haveLowMorale = morale <= 0;
+        }
+    }
+}
